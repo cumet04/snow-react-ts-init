49684b94d31fd3b8940984519edd9f9f41a275e0
cumet04 <medalhkr@gmail.com>
2020-06-20 03:44:47 +0900

    css modules
    https://www.snowpack.dev/#import-css-modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 interface AppProps {}
 
 function App({}: AppProps) {
   return (
-    <div className="App">
-      <header className="header">
-        <img src={logo} className="logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="link"
+          className={styles.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
